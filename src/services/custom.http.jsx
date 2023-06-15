0bb01d7ecb9cf8d58e002518436693a3096bc949
matheusfelipe1 <matheusfel13@gmail.com>
2023-06-15 09:44:03 -0300

    fix

--- src/services/custom.http.jsx
+++ src/services/custom.http.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default class CustomHttp {
     client = () => {
         return axios.create({
-            baseURL: 'http://127.0.0.1:9300/inotas-59ff4/us-central1/api',
+            baseURL: 'https://us-central1-inotas-59ff4.cloudfunctions.net/api',
             headers: {
                 'Authorization': `Bearer ${this.getToken()}`
             },
